57cc7a1f69d5ebe333133efcf7d9e6215dc7f625
Chris Pettitt <chris@samsarin.com>
2010-02-26 00:17:45 -0800

    Switch to strict ByteStrings
    Strict ByteStrings seem to yield better performance for my test case, which is:
    
    - Insert 32K unique random english words
    - Lookup each word 100 times (3.2M lookups)
    
    With -O2, I get the following numbers:
    
    Strict ByteString 1.276s
    [Char]            2.176s
    Lazy ByteString   3.449s

--- Data/PTree/PTree.hs
+++ Data/PTree/PTree.hs
@@ -1,7 +1,7 @@
 module Data.PTree.PTree where
 
 import Control.Arrow (first)
-import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Char8 as C
 import Data.Char (ord)
 import qualified Data.Vector as V
 import Prelude hiding (foldr, lookup, null)
