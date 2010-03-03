ae5c236699daa4c1439eb6a6fa4da5a45c4f8d51
Chris Pettitt <chris@samsarin.com>
2010-03-03 06:26:26 -0800

    Expand test suite
    Now we generate random PTrees for use with our tests. This change also adds
    a number of new tests.

--- test/Main.hs
+++ test/Main.hs
@@ -2,31 +2,49 @@
 
 module Main where
 
+import Control.Monad (liftM)
 import qualified Data.ByteString.Char8 as C
-import Data.PTree as P
+import Data.List ((\\))
+import qualified Data.List as L
+import Data.PTree
 import Prelude hiding (lookup, null)
 import Test.QuickCheck
 import Text.Printf
 
-instance Arbitrary P.Key where
+instance Arbitrary a => Arbitrary (PTree a) where
+    arbitrary = liftM fromList arbitrary
+
+instance CoArbitrary a => CoArbitrary (PTree a) where
+    coarbitrary = coarbitrary . toList
+
+instance Arbitrary Key where
     arbitrary = C.pack `fmap` listOf (choose ('a', 'z'))
 
-instance CoArbitrary P.Key where
+instance CoArbitrary Key where
     coarbitrary = coarbitrary . C.unpack
 
-type V = Int
+type T = PTree Int
 
-prop_member_empty k = P.notMember k P.empty
+prop_member_empty k = notMember k empty
 
-prop_insert_one k (v :: V) = P.lookup k (P.insert k v P.empty) == Just v
+prop_not_member (t :: T) k = not (notMember k t && member k t)
 
-prop_change_one k (v1 :: V) (v2 :: V) =
-    v1 /= v2 ==>
-        P.lookup k (P.insert k v2 $ P.insert k v1 P.empty) == Just v2
+prop_insert (t :: T) k v = lookup k (insert k v t) == Just v
 
-tests = [("member_empty", quickCheck prop_member_empty)
-        ,("insert_one", quickCheck prop_insert_one)
-        ,("change_one", quickCheck prop_change_one)
-        ]
+prop_insert_idem (t :: T) k v = insert k v t == insert k v (insert k v t)
 
-main = mapM_ (\(s, a) -> printf "%-25s: " s >> a) tests
+prop_from_to_list (t :: T) = fromList (toList t) == t
+
+prop_keys (t :: T) = L.null (keys t \\ keyList) && L.null (keyList \\ keys t)
+    where
+        keyList = map fst $ toList t
+
+main = do
+    let check s a = printf "%-25s: " s >> quickCheck a
+
+    check "member_empty"    prop_member_empty
+    check "not_member"      prop_not_member
+    check "insert"          prop_insert
+    check "insert_idem"     prop_insert_idem
+    check "from_to_list"    prop_from_to_list
+    check "keys"            prop_keys
